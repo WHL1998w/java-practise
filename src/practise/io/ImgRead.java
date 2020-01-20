44656b560571ae1912b0d48834cd868c183c67b4
wanghuanle <wanghuanle@qq.com>
2020-01-20 11:23:50 +0800

    按文件大小排序输出

---
+++ src/practise/io/ImgRead.java
@@ -0,0 +1,82 @@
+package practise.io;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @ClassName ImgRead
+ * @Description 文件以大小输出
+ * @Author wanghuanle
+ * @Date 2020/1/20
+ **/
+public class ImgRead {
+    public static void main(String[] args) {
+        //封装目录
+        File srcFile = new File("D:\\javastudy\\work");
+        //递归功能实现
+        System.out.println("-------------不按大小排序输出------------");
+        getAllJavaFilePaths(srcFile);
+        System.out.println("--------------按大小排序输出-------------");
+        orderByLength(srcFile);
+    }
+
+    /**
+     * 按文件大小排序输出
+     *
+     * @param srcFile
+     */
+    public static void orderByLength(File srcFile) {
+        List<File> files = Arrays.asList(new File(String.valueOf(srcFile)).listFiles());
+        Collections.sort(files, new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                long diff = f1.length() - f2.length();
+                if (diff > 0) {
+                    return 1;
+                } else {
+                    if (diff == 0) {
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+
+            public boolean equals(Object obj) {
+                return true;
+            }
+        });
+        for (File f : files) {
+            if (f.isDirectory()) {
+                continue;
+            }
+            System.out.println(f.getAbsolutePath() + ":" + f.length());
+        }
+    }
+
+    /**
+     * 按路径默认文件排序输出（不按文件大小排序输出）
+     *
+     * @param srcFile
+     */
+    private static void getAllJavaFilePaths(File srcFile) {        //获取该目录下所有的文件或者文件夹的File数组
+        File[] fileArray = srcFile.listFiles();
+        //遍历该File数组，得到每一个File对象
+        for (File file : fileArray) {
+            //判断该File对象是否是文件夹
+            if (file.isDirectory()) {
+                getAllJavaFilePaths(file);
+            } else {
+                //继续判断是否是以.png结尾
+                if (file.getName().endsWith(".png")) {
+                    //就输出该文件的绝对路径
+                    System.out.println(file.getAbsolutePath());
+                }
+            }
+        }
+
+    }
+}
